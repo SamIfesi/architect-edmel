b5af7d47f0d461375eeb3c52989a4938df656cc8
SamIfesi <Ekesamrose@gmail.com>
2026-09-02 16:00:42 +0100

    Remove unused files and components to streamline the project

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-primary">
-      <main className="flex flex-1 w-full max-w-6xl flex-col items-center justify-between sm:items-start">hello world</main>
-    </div>
-  );
-}
