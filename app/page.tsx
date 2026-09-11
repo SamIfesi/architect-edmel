f85327985f25d9d7bc0ddda4804c3bae64d462c2
SamIfesi <Ekesamrose@gmail.com>
2026-09-11 15:41:41 +0100

    Replace Hero component with Navbar in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Hero from '../src/components/Hero';
+import Navbar from '../src/components/nav/Navbar';
+
 export default function Home() {
   return (
     <main className="flex flex-col flex-1 items-center justify-center bg-primary">
       <div className="flex flex-1 w-full max-w-6xl flex-col items-center justify-between sm:items-start">
-        <Hero />
+        <Navbar />
       </div>
     </main>
   );
