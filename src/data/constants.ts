72ab32d45fcce1d607d570086f5bcfedaa7c5a80
SamIfesi <Ekesamrose@gmail.com>
2026-09-11 15:41:10 +0100

    Add NAV_LINKS constant for navigation structure

---
+++ src/data/constants.ts
@@ -0,0 +1,17 @@
+export const  NAV_LINKS = [
+  {
+    name: 'Home',
+    href: '/',
+    icon: 'Home',
+  },
+  {
+    name: 'About',
+    href: '/about',
+    icon: '',
+  },
+  {
+    name: 'Contact',
+    href: '/contact',
+    icon: '',
+  },
+];
