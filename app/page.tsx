7217502b349498ebb7a7f01cc9ba20db5adf2894
SamIfesi <Ekesamrose@gmail.com>
2026-09-13 20:48:55 +0100

    Update Home component: include Services component in the main layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Navbar from '@/components/ui/Navbar';
 import Footer from '@/components/ui/Footer';
 import Hero from '@/components/home/Hero';
+import Services from '@/components/home/Services';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col flex-1 items-center justify-center bg-">
+      <main className="flex flex-col flex-1 items-center justify-center">
         <Hero />
+        <Services />
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
